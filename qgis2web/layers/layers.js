fe5eea2a69a641e3aefcbcd9bf861d2a55b772af
Kirby Bartlett <kirbybartlett@ESM.UCSB.EDU>
2019-11-07 20:50:55 -0800

    adding everything

---
+++ qgis2web/layers/layers.js
@@ -0,0 +1,42 @@
+var wms_layers = [];
+
+var format_bt_range_2019_0 = new ol.format.GeoJSON();
+var features_bt_range_2019_0 = format_bt_range_2019_0.readFeatures(json_bt_range_2019_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_bt_range_2019_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_bt_range_2019_0.addFeatures(features_bt_range_2019_0);
+var lyr_bt_range_2019_0 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_bt_range_2019_0, 
+                style: style_bt_range_2019_0,
+                interactive: true,
+                title: '<img src="styles/legend/bt_range_2019_0.png" /> bt_range_2019'
+            });
+var format_BullTrout_Lakes_1 = new ol.format.GeoJSON();
+var features_BullTrout_Lakes_1 = format_BullTrout_Lakes_1.readFeatures(json_BullTrout_Lakes_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_BullTrout_Lakes_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_BullTrout_Lakes_1.addFeatures(features_BullTrout_Lakes_1);
+var lyr_BullTrout_Lakes_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_BullTrout_Lakes_1, 
+                style: style_BullTrout_Lakes_1,
+                interactive: true,
+                title: '<img src="styles/legend/BullTrout_Lakes_1.png" /> BullTrout_Lakes'
+            });
+
+lyr_bt_range_2019_0.setVisible(true);lyr_BullTrout_Lakes_1.setVisible(true);
+var layersList = [lyr_bt_range_2019_0,lyr_BullTrout_Lakes_1];
+lyr_bt_range_2019_0.set('fieldAliases', {'FID_bt_dis': 'FID_bt_dis', 'BTID': 'BTID', 'NAME': 'NAME', 'OCCUPANCY': 'OCCUPANCY', 'STATE': 'STATE', 'STATE2': 'STATE2', 'H_USE_TYPE': 'H_USE_TYPE', 'SCALE': 'SCALE', 'HYDRO_SRC': 'HYDRO_SRC', 'EDIT_TYPE': 'EDIT_TYPE', 'EDIT_COMNT': 'EDIT_COMNT', 'BT_SOURCE': 'BT_SOURCE', 'WaWaterTyp': 'WaWaterTyp', 'FWS_OFFICE': 'FWS_OFFICE', 'Review': 'Review', 'FWSID': 'FWSID', 'PROP_CH': 'PROP_CH', 'New': 'New', 'LocalPop': 'LocalPop', 'FID_Core_A': 'FID_Core_A', 'Core_Area': 'Core_Area', 'State_1': 'State_1', 'Recovery_U': 'Recovery_U', 'Management': 'Management', 'CHSU': 'CHSU', 'CHU': 'CHU', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', 'Acres': 'Acres', 'Hectares': 'Hectares', 'LP_Name': 'LP_Name', 'Notes': 'Notes', 'RecovUnit': 'RecovUnit', 'MultiPops': 'MultiPops', 'InCOED': 'InCOED', 'Geo_Source': 'Geo_Source', 'OldPopID': 'OldPopID', 'NewPopID': 'NewPopID', 'CoreArea11': 'CoreArea11', 'WB_ID': 'WB_ID', '2005_CH': '2005_CH', '2010_CH': '2010_CH', 'TribTo': 'TribTo', 'HUC_4': 'HUC_4', 'YEAR_ADDED': 'YEAR_ADDED', 'ADDED_FROM': 'ADDED_FROM', 'DETERMINAT': 'DETERMINAT', 'Source': 'Source', 'CONFIDENCE': 'CONFIDENCE', });
+lyr_BullTrout_Lakes_1.set('fieldAliases', {'BTID': 'BTID', 'Name': 'Name', 'Prop_CH': 'Prop_CH', 'State': 'State', 'RecoveryUn': 'RecoveryUn', 'CHU_Num': 'CHU_Num', 'CHU': 'CHU', 'CHSU': 'CHSU', 'H_USE_TYPE': 'H_USE_TYPE', 'OCCUPANCY': 'OCCUPANCY', 'Acres': 'Acres', 'Hectares': 'Hectares', 'FCH': 'FCH', 'Exclusion': 'Exclusion', });
+lyr_bt_range_2019_0.set('fieldImages', {'FID_bt_dis': 'Range', 'BTID': 'TextEdit', 'NAME': 'TextEdit', 'OCCUPANCY': 'TextEdit', 'STATE': 'TextEdit', 'STATE2': 'TextEdit', 'H_USE_TYPE': 'TextEdit', 'SCALE': 'Range', 'HYDRO_SRC': 'TextEdit', 'EDIT_TYPE': 'Range', 'EDIT_COMNT': 'TextEdit', 'BT_SOURCE': 'TextEdit', 'WaWaterTyp': 'TextEdit', 'FWS_OFFICE': 'TextEdit', 'Review': 'Range', 'FWSID': 'Range', 'PROP_CH': 'Range', 'New': 'Range', 'LocalPop': 'Range', 'FID_Core_A': 'Range', 'Core_Area': 'TextEdit', 'State_1': 'TextEdit', 'Recovery_U': 'TextEdit', 'Management': 'TextEdit', 'CHSU': 'TextEdit', 'CHU': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', 'Acres': 'TextEdit', 'Hectares': 'TextEdit', 'LP_Name': 'TextEdit', 'Notes': 'TextEdit', 'RecovUnit': 'TextEdit', 'MultiPops': 'Range', 'InCOED': 'TextEdit', 'Geo_Source': 'TextEdit', 'OldPopID': 'Range', 'NewPopID': 'Range', 'CoreArea11': 'TextEdit', 'WB_ID': 'Range', '2005_CH': 'TextEdit', '2010_CH': 'TextEdit', 'TribTo': 'TextEdit', 'HUC_4': 'TextEdit', 'YEAR_ADDED': 'Range', 'ADDED_FROM': 'TextEdit', 'DETERMINAT': 'Range', 'Source': 'TextEdit', 'CONFIDENCE': 'Range', });
+lyr_BullTrout_Lakes_1.set('fieldImages', {'BTID': 'TextEdit', 'Name': 'TextEdit', 'Prop_CH': 'Range', 'State': 'TextEdit', 'RecoveryUn': 'TextEdit', 'CHU_Num': 'Range', 'CHU': 'TextEdit', 'CHSU': 'TextEdit', 'H_USE_TYPE': 'TextEdit', 'OCCUPANCY': 'TextEdit', 'Acres': 'TextEdit', 'Hectares': 'TextEdit', 'FCH': 'Range', 'Exclusion': 'TextEdit', });
+lyr_bt_range_2019_0.set('fieldLabels', {'FID_bt_dis': 'no label', 'BTID': 'no label', 'NAME': 'no label', 'OCCUPANCY': 'no label', 'STATE': 'no label', 'STATE2': 'no label', 'H_USE_TYPE': 'no label', 'SCALE': 'no label', 'HYDRO_SRC': 'no label', 'EDIT_TYPE': 'no label', 'EDIT_COMNT': 'no label', 'BT_SOURCE': 'no label', 'WaWaterTyp': 'no label', 'FWS_OFFICE': 'no label', 'Review': 'no label', 'FWSID': 'no label', 'PROP_CH': 'no label', 'New': 'no label', 'LocalPop': 'no label', 'FID_Core_A': 'no label', 'Core_Area': 'no label', 'State_1': 'no label', 'Recovery_U': 'no label', 'Management': 'no label', 'CHSU': 'no label', 'CHU': 'no label', 'Shape_Leng': 'no label', 'Shape_Area': 'no label', 'Acres': 'no label', 'Hectares': 'no label', 'LP_Name': 'no label', 'Notes': 'no label', 'RecovUnit': 'no label', 'MultiPops': 'no label', 'InCOED': 'no label', 'Geo_Source': 'no label', 'OldPopID': 'no label', 'NewPopID': 'no label', 'CoreArea11': 'no label', 'WB_ID': 'no label', '2005_CH': 'no label', '2010_CH': 'no label', 'TribTo': 'no label', 'HUC_4': 'no label', 'YEAR_ADDED': 'no label', 'ADDED_FROM': 'no label', 'DETERMINAT': 'no label', 'Source': 'no label', 'CONFIDENCE': 'no label', });
+lyr_BullTrout_Lakes_1.set('fieldLabels', {'BTID': 'no label', 'Name': 'no label', 'Prop_CH': 'no label', 'State': 'no label', 'RecoveryUn': 'no label', 'CHU_Num': 'no label', 'CHU': 'no label', 'CHSU': 'no label', 'H_USE_TYPE': 'no label', 'OCCUPANCY': 'no label', 'Acres': 'no label', 'Hectares': 'no label', 'FCH': 'no label', 'Exclusion': 'no label', });
+lyr_BullTrout_Lakes_1.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
